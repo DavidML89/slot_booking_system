aa108f2427c1895e0a8c4e5a67d2648760bb534d
David Morio <david.morio.lesavourey@gmail.com>
2022-05-03 19:26:38 +0200

    Check slot availibility method in progress

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+# Create UUID
 gem 'uuid', '~> 2.3', '>= 2.3.9'
 
 # React gems
--- app/javascript/components/SlotsOrganizer.jsx
+++ app/javascript/components/SlotsOrganizer.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import NewBookingForm from "./NewBookingForm";
 import Slot from "./Slot";
-import moment from 'moment';
+import Moment from 'moment';
+import { extendMoment } from 'moment-range';
 import {v4 as uuid} from "uuid";
+
+const moment = extendMoment(Moment);
 
 class SlotsOrganizer extends Component {
   constructor(props) {
@@ -22,9 +25,48 @@ class SlotsOrganizer extends Component {
     then((bookings) => this.setState( { bookings }));
   }
 
+  dateRangeOverlaps(a_start, a_end, b_start, b_end) {
+    if (a_start <= b_start && b_start <= a_end) return true; // b starts in a
+    if (a_start <= b_end   && b_end   <= a_end) return true; // b ends in a
+    if (b_start <  a_start && a_end   <  b_end) return true; // a in b
+    return false;
+  }
+  multipleDateRangeOverlaps(dates) {
+    let i, j;
+    console.log(dates.lenght)
+    if (dates.length % 2 !== 0)
+    throw new TypeError('dates length must be a multiple of 2');
+    for (i = 0; i < dates.length - 2; i += 2) {
+      for (j = i + 2; j < dates.length; j += 2) {
+        if (
+          this.dateRangeOverlaps(
+            dates[i], dates[i+1],
+            dates[j], dates[j+1]
+            )
+            ) return true;
+          }
+        }
+        return false;
+      }
+
+    createBookingArray(bookings) {
+      let i;
+      let bookingsArray = [];
+      // const bookingArray = Object.values(bookings);
+      bookings.map((booking) => {
+        bookingsArray.push(Object.values(booking))
+      })
+      let bookingArr = []
+      bookingsArray.map((arr) => {
+        bookingArr.push(arr[1], arr[2])
+      })
+      return bookingArr;
+    }
+
   createSlot(slot) {
     // fetch the bookings
     const bookings = this.state.bookings;
+    const bookingArr = this.createBookingArray(bookings)
     // initialise the beginning and last time for the user input date
     let beginningSlot = new moment.utc(`${slot.date}T00:00:00.000Z`);
     const lastSlot = new moment.utc(`${slot.date}T23:45:00.000Z`);
@@ -39,19 +81,26 @@ class SlotsOrganizer extends Component {
       let endSlot = beginningSlot.add(duration)
       let end = new moment.utc(endSlot)
       // map through the bookings to check if slot is available
+      console.log(this.multipleDateRangeOverlaps(bookingArr))
       bookings.map(booking => {
-        let endBooking = new moment.utc(booking.end_datetime);
-        // console.log(end_datetime);
-        // console.log(beginning);
         let startBooking = new moment.utc(booking.start_datetime);
+        let endBooking = new moment.utc(booking.end_datetime);
+        let rangeBooking = moment.range(startBooking, endBooking)
+        console.log(rangeBooking);
+        // console.log(beginning);
         // console.log(beginning.format());
         // if beginning and end of a slot is not included in a booking
-        if (beginning > endBooking && end < startBooking ) {  //
-          return console.log('created');
-        }
+        console.log(!rangeBooking.contains(beginning))
+        console.log(!rangeBooking.contains(end))
+        // if (!rangeBooking.contains(beginning) && !rangeBooking.contains(end)) {
+        //     // return console.log('created');
+        //     slot = { start_datetime: beginning, end_datetime: end, id: uuid() }
+        //     slots.push(slot);
+        // }
+        // if (beginning > endBooking && end < startBooking ) {  //
+        //   return console.log('created');
+        // }
       })
-      slot = { start_datetime: beginning, end_datetime: end, id: uuid() }
-      slots.push(slot);
       beginningSlot.subtract(duration).add(15, 'minutes')
     }
     this.setState(state => ({
@@ -64,13 +113,9 @@ class SlotsOrganizer extends Component {
   handleBooking(id) {
     const slot = this.state.slots.map((slot) => {
       if (slot.id === id) {
-        console.log(slot);
         const start_datetime = slot.start_datetime.format()
-        console.log(start_datetime);
         const end_datetime = slot.end_datetime.format()
         const selectedSlot = { id, start_datetime, end_datetime }
-        console.log(selectedSlot);
-
         fetch('/api/v1/bookings/', {
           method: 'post',
           headers: { 'Content-Type': 'application/json' },
